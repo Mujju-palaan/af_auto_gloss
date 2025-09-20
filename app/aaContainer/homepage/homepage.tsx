6cfef9be74e84d012e49533bb5f093fcf4661908
Mujju-palaan <mdmujahid97@gmail.com>
2025-09-21 04:20:49 +0530

    content done

--- app/aaContainer/homepage/homepage.tsx
+++ app/aaContainer/homepage/homepage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import StatsContainer from '@/components/StatsContainer'
 import Newsletter from '@/components/Newsletter'
+import Content from './content'
 
 const HomePage = () => {
   return (
     <div className='bg-white 500'>
+      <Content/>
       <StatsContainer/>
       
       <Newsletter />
