0f19e74e8cec1609353428cce72b4a047483482a
Mujju-palaan <mdmujahid97@gmail.com>
2025-09-21 03:13:23 +0530

    product details page done

---
+++ app/product_details/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Product_detailsPage from '../aaContainer/product_ details/product_detailpage'
+
+const Product = () => {
+  return (
+    <div>
+      <Product_detailsPage/>
+    </div>
+  )
+}
+
+export default Product
