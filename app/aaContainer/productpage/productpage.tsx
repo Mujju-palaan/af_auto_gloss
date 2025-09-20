13faa65abdf1347e99390045349706f3f5c9c978
Mujju-palaan <mdmujahid97@gmail.com>
2025-09-21 02:37:40 +0530

    product page completed

--- app/aaContainer/productpage/productpage.tsx
+++ app/aaContainer/productpage/productpage.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Productcard1 from '../../../components/product/Productcard1'
 
 const ProductPage = () => {
   return (
-    <div>ProductPage</div>
+    <div className='bg-white 500'>
+      <Productcard1/>
+    </div>
   )
 }
 
---
+++ data/product1.ts
@@ -0,0 +1,35 @@
+export const Product1 = [
+  {
+    id: 1,
+    src: "https://dummyimage.com/400x400",
+    title: "Smart Phones",
+    reviews:'4',
+    product_description:'Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.',
+    price:'₹400'
+  },
+  {
+    id: 2,
+    src: "https://dummyimage.com/400x400",
+    title: "Smart Phones",
+    reviews:'4',
+    product_description:'Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.',
+    price:'₹400'
+  },
+  {
+    id: 3,
+    src: "https://dummyimage.com/400x400",
+    title: "Smart Phones",
+    reviews:'4',
+    product_description:'Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.',
+    price:'₹400'
+  },
+  {
+    id: 4,
+    src: "https://dummyimage.com/400x400",
+    title: "Smart Phones",
+    reviews:'4',
+    product_description:'Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.',
+    price:'₹400'
+  },
+  
+];
