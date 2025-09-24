2f3d4af2ddcccbe514cdd6aa09d3802c9f892700
Mujju-palaan <mdmujahid97@gmail.com>
2025-09-24 18:57:58 +0530

    all routing, product items removed

--- app/aaContainer/homepage/homepage.tsx
+++ app/aaContainer/homepage/homepage.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import StatsContainer from '@/components/StatsContainer'
 import Newsletter from '@/components/Newsletter'
 import Content from './content'
-import Product from '@/app/product/page'
 
 const HomePage = () => {
   return (
     <div className='bg-white 500'>
       <Content/>
-      <Product/>
       <StatsContainer/>
       
       <Newsletter />
