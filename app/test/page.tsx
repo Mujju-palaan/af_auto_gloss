5bcb51fece6f6b17b8f2abb5027375ae272a3dcd
Mujju-palaan <mdmujahid97@gmail.com>
2025-09-30 03:12:45 +0530

    product details done

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Productall from '../aaContainer/product/product'
+
+const page = () => {
+  return (
+    <div>
+        <Productall/>
+    </div>
+  )
+}
+
+export default page
