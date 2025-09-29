d237728c829102f5c88cb5075f5d409a58327d62
Mujju-palaan <mdmujahid97@gmail.com>
2025-09-30 02:04:25 +0530

    coming soon done

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComingSoon from '@/components/cards/CommingSoon'
+
+const page = () => {
+  return (
+    <div>
+        <ComingSoon/>
+    </div>
+  )
+}
+
+export default page
