445f72b83c44299c03e875e4c1758786b192ca90
Mujju-palaan <mdmujahid97@gmail.com>
2026-01-24 22:05:03 +0530

    useSearchParams() should be wrapped in a suspense boundary

--- app/(main)/product/page.tsx
+++ app/(main)/product/page.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import ProductCardsOnly from '../../../components/product/ProductCardsOnly'
-import Productdetailpage from '../../../components/product/Productdetailpage'
-
+import { Suspense } from "react";
+import ProductCardsOnly from "../../../components/product/ProductCardsOnly";
+import Productdetailpage from "../../../components/product/Productdetailpage";
 
 const page = () => {
+  const ProductDetailSkeleton = () => (
+    <div className="animate-pulse p-10 text-white">
+      Loading product details…
+    </div>
+  );
   return (
     <div>
       <ProductCardsOnly />
-      <Productdetailpage />
-      
+      <Suspense fallback={<ProductDetailSkeleton />}>
+        <Productdetailpage />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
