63dfff9d0d7357c9c73208adee7bd811f39225c6
Mujju-palaan <mdmujahid97@gmail.com>
2026-01-27 18:30:31 +0530

    price updated

--- data/ProductData.js
+++ data/ProductData.js
@@ -4,7 +4,7 @@ export const ProductData = [
     image: "/product/Product_Foam.jpg",
     title: "Snow Foam Shampoo",
     reviews: "5",
-    price: "600.00",
+    price: "800.00",
     product_description: {
       summary:
         "Our Foam Wash is a naturally formulated car shampoo that effectively removes dirt, dust, and road grime without stripping existing paint protection or dulling the vehicle’s finish. Its gentle yet powerful cleansing action ensures a safe wash experience while maintaining the original gloss and integrity of automotive surfaces.",
@@ -24,7 +24,7 @@ export const ProductData = [
     image: "/product/Product_Dashboard_Polish.jpg",
     title: "Dashboard Poish",
     reviews: "5",
-    price: "1000.00",
+    price: "1200.00",
     product_description: {
       summary:
         "Designed to restore and protect interior surfaces, our Dashboard Polish enhances the natural appearance of dashboards while helping prevent cracking, fading, and premature aging. Its refined formulation delivers a clean, well-maintained look without compromising surface integrity, making it ideal for regular interior care.",
@@ -42,7 +42,7 @@ export const ProductData = [
     image: "/product/Product_TyrePolish.png",
     title: "Tyre Polish",
     reviews: "5",
-    price: "1200.00",
+    price: "1400.00",
     product_description: {
       summary:
         "Our Tyre Polish delivers a deep black shine while nourishing the rubber, helping extend tyre life without the use of harmful chemicals. Its gentle yet effective formulation enhances appearance while maintaining the flexibility and integrity of tyre surfaces, making it suitable for regular use.",
@@ -81,7 +81,7 @@ export const ProductData = [
     image: "/product/Product_GlassCleaner.jpg",
     title: "Glass Cleaner",
     reviews: "5",
-    price: "500.00",
+    price: "600.00",
     product_description: {
       summary:
         "Glass Cleaner is a powerful yet safe cleaning solution designed to remove dirt, fingerprints, water stains, and road grime from vehicle glass surfaces without leaving streaks or residue. Its effective formulation delivers clear, spotless results while being gentle on all types of automotive glass, enhancing both visibility and appearance.",
