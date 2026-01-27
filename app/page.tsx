37f4e7faf00021056c0d71a12a30bc94b0890846
Mujju-palaan <mdmujahid97@gmail.com>
2026-01-27 20:22:02 +0530

    home page, hero section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
-import ProductCardsOnly from '@/components/product/ProductCardsOnly';
-import Content from '../components/home/Content';
-import Home_HeroSection from '../components/home/Home_HeroSection'
-import Status from '../components/home/Stats'
-import Banner from '../components/home/Banner'
+import ProductCardsOnly from "@/components/product/ProductCardsOnly";
+import Content from "../components/home/Content";
+import Home_HeroSection from "../components/home/Home_HeroSection";
+import Status from "../components/home/Stats";
+import Banner from "../components/home/Banner";
 
-const  Page = ()  => {
+const Page = () => {
   return (
-    <div className='text-white/80'>
+    <div className="text-white/80">
       <Banner />
+      <h1 className="flex md:text-5xl text-xl font-semibold text-red-600 
+      justify-center items-center text-center md:px-32 px-8 md:mt-20 mt-6">
+        Why Professionals Choose AF Autogloss for Regular Car Washing ?
+      </h1>
       <Home_HeroSection />
       <ProductCardsOnly />
       <Content />
-      
+
       <Status />
-      
     </div>
   );
-}
+};
 
-export default Page
+export default Page;
--- components/home/Home_HeroSection.jsx
+++ components/home/Home_HeroSection.jsx
@@ -5,15 +5,49 @@ const Home_HeroSection = () => {
   return (
     <div className="flex flex-col-reverse md:flex-row items-center gap-6 md:px-20 m-2">
       {/* Text Section */}
-      <div className="md:w-1/2 w-full p-4">
-        <h1 className="md:text-4xl text-xl font-semibold text-red-600">
-          Why is Regular Car Wash Important for your Vehicle?
-        </h1>
-        <p className="mt-4 md:text-[16px] text-[12px] ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-          doloremque saepe architecto maiores repudiandae amet perferendis
-          repellendus, reprehenderit voluptas sequi.
-        </p>
+      <div className="md:w-1/2 w-full md:pt-16 p-4">
+        
+        <h3 className="md:text-xl text-red-300 mt-2">
+          Natural Auto Care Products Manufactured in India.
+        </h3>
+
+        <h3 className="md:text-xl text-red-300">
+          Naturally Made Auto Care Products — Safe for Cars, Trusted by
+          Professionals.
+        </h3>
+
+        <ul className="list-disc m-4 text-[14px]">
+          <li>
+            We are an Indian manufacturer of high-quality auto care products,
+            developed using carefully selected raw materials and
+            <strong className="text-red-300">
+              {" "}
+              natural formulations without harmful chemicals
+            </strong>
+            .
+          </li>
+          <li>
+            Our products are designed to meet the daily needs of
+            <strong className="text-red-300">
+              {" "}
+              car dealers, service centres, workshops, and car wash businesses,
+            </strong>{" "}
+            ensuring effective cleaning, superior shine, and complete safety for
+            vehicle paint, interiors, and rubber parts.
+          </li>
+          <li>
+            With a
+            <strong className="text-red-300"> 1:10 dilution ratio,</strong> our
+            products offer
+            <strong className="text-red-300">
+              {" "}
+              high performance with low usage cost,
+            </strong>{" "}
+            helping businesses reduce expenses while delivering premium results
+            to customers.
+          </li>
+          <li className="text-red-300">Safe for Vehicles. Smart for Business. Made in India.</li>
+        </ul>
       </div>
 
       {/* Image Section */}
